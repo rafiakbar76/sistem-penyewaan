2e75292b942fb281005d4731e7cfd332cc33dfaa
rafiakbar76 <96912010+rafiakbar76@users.noreply.github.com>
2023-11-21 11:45:27 +0700

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,46 @@
+<div class="switcher-wrapper">	
+    <div class="demo_changer" style="left: auto">
+        <div class="demo-icon customBgColor"><i class="fa fa-cog fa-spin fa-2x"></i></div>
+		<div class="form_holder">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="predefined_styles">
+                        <div class="skin-theme-switcher">
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">			
+				<li class="ts-label">
+				<img style="width: 58px; position: relative; top: 6px; left: 10px;" src="img/Group 105.png" alt="">
+				</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li><a href="#"><i class="fa fa-exchange"></i> Sewa</a>
+					<ul class="ts-sidebar-menu">
+						<li><a href="sewa_bayar.php">Menunggu Pembayaran</a></li>				  						
+						<li><a href="sewa_konfirmasi.php">Menunggu Konfirmasi</a></li>
+						<li><a href="sewa_kembali.php">Pengembalian</a></li>
+						<li><a href="sewa.php">Data Sewa</a></li>
+                    </ul>
+				</li>
+				<li><a href="#"><i class="fa fa-car"></i> Mobil</a>
+					<ul class="ts-sidebar-menu">
+						<li><a href="merek.php">Data Merek</a></li>				  						
+						<li><a href="mobil.php">Data Mobil</a></li>
+                    </ul>
+				</li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> User</a></li>
+				<li><a href="car-owner.php"><img src="img/card-account.png" style="width: 20px; height: 20px; top: 0px; position: relative; left: -3px;">car owner</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-phone"></i> Menghubungi</a></li>
+				<li><a href="manage-pages.php"><i class="fa fa-gear"></i> Kelola Halaman</a></li>
+				<li><a href="update-contactinfo.php"><i class="fa fa-info"></i> Kontak Info</a></li>
+				<li><a href="laporan.php"><i class="fa fa-files-o"></i> Laporan</a></li>
+				<li><a href="logout.php"><li style="width: 253px; height: 48px; left: -3px; top: 436px; position: absolute; background: #001942">
+				<img src="img/Logout.png" style="width: 15px; position: relative; left: -3px; top: -1px;">Logout</a></li>
+</li>
+			</ul>
+	</nav>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
