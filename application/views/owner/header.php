187bd81eeaaadd233a8e305979045f0e2c25a151
Abdullah Rusyda Maskur <96782892+AbdullahRusydaMaskur@users.noreply.github.com>
2023-11-28 06:28:44 +0700

    Add files via upload

---
+++ application/views/owner/header.php
@@ -0,0 +1,32 @@
+   <!-- header -->
+   <header class="main-header">
+        <!-- Logo -->
+        
+
+        <!-- nav -->
+        <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <div class="avatar">
+        <img src="http://localhost/sistem-penyewaan/assets/img/avatar.png" alt="owner" class="user">
+        <div class="text-wrapper">Nusantara Rental</div>
+        </div>
+        
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+            <!-- User Account Menu -->
+            <li class="dropdown user user-menu">
+                <!-- Menu Toggle Button -->
+                <a href="http://localhost/sistem-penyewaan/assets/#" class="dropdown-toggle" data-toggle="dropdown">
+                <!-- The user image in the navbar-->
+                <!-- <img src="http://localhost/eplusgo_penjualan/assets/img/IMG_20211218_1805051.jpg" class="user-image" alt="User Image"> -->
+                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                
+                </a>
+                
+            </li>
+            </ul>
+        </div>
+        </nav>
+    </header> <!-- nav -->
+      
