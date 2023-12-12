79adbd673e771bedaeec63656f73baf5011750e4
rafiakbar76 <96912010+rafiakbar76@users.noreply.github.com>
2023-12-12 08:24:03 +0700

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,25 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">			
+				<li class="ts-label">
+				<img style="width: 58px; position: relative; top: 6px; left: 10px;" src="img/Group 105.png" alt="">
+				</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li><a href="#"><i class="fa fa-exchange"></i> Sewa</a>
+					<ul class="ts-sidebar-menu">
+						<li><a href="pemesanan.php">Pemesanan</a></li>				  						
+						<li><a href="sewa_konfirmasi.php">Pembayaran</a></li>
+						<li><a href="sewa_kembali.php">Pengembalian</a></li>
+                    </ul>
+				</li>
+				<li><a href="#"><i class="fa fa-car"></i> Mobil</a>
+					<ul class="ts-sidebar-menu">			  						
+						<li><a href="mobil.php">Data Mobil</a></li>
+                    </ul>
+				</li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> User</a></li>
+				<li><a href="car-owner.php"><img src="img/card-account.png" style="width: 20px; height: 20px; top: 0px; position: relative; left: -3px;">car owner</a></li>
+				<li><a href="logout.php">
+				<img src="img/Logout.png" style="width: 15px; position: relative; left: -3px; top: -1px;">Logout</a></li>
+				</li>
+			</ul>
+	</nav>
