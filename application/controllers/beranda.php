eba9a0be624ac96bfb58f0478aaa08e981bec502
Abdullah Rusyda Maskur <96782892+AbdullahRusydaMaskur@users.noreply.github.com>
2023-12-23 00:06:33 +0700

    Update beranda.php

--- application/controllers/beranda.php
+++ application/controllers/beranda.php
@@ -9,6 +9,9 @@ class Beranda extends CI_Controller {
     
     public function index()
     {
+        $data=[
+            'dataOwner'=>$this->m_penyewa->detail()->row()
+        ];
         $data['dataPenyewa']= $this->m_penyewa->select_all();
         $this->load->view("owner/sidebar");
         $this->load->view("owner/header");
