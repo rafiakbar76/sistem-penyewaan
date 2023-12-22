59b535ac1eb08e459f422ea5c7d18e84a0b2271a
Abdullah Rusyda Maskur <96782892+AbdullahRusydaMaskur@users.noreply.github.com>
2023-12-23 02:02:36 +0700

    Add files via upload

---
+++ application/views/owner/sidebar.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>AdminCRUD | Dashboard</title>
+    <!-- meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- css --> 
+    <link rel="stylesheet" href="http://localhost/sistem-penyewaan/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/sistem-penyewaan/assets/css/clearfix.css">
+    <link rel="stylesheet" href="http://localhost/sistem-penyewaan/assets/css/resetcss.css">
+    <style>
+        
+    </style>
+    <!-- jQuery 2.2.3 -->
+    <script src="http://localhost/eplusgo_penjualan/assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+  </head>
+
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+   
+      <!-- sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+       
+
+        <!-- Sidebar user panel (optional) -->
+        <a href="http://localhost/sistem-penyewaan/"><div class="label"><img class="RENTAJA" src="http://localhost/sistem-penyewaan/assets/img/RENTAJA.png" /></div></a>
+        <!-- <div class="label"><img class="RENTAJA" src="http://localhost/sistem-penyewaan/assets/img/RENTAJA.png" /></div> -->
+        <!-- Sidebar Menu -->
+        <div class="menu">
+        <ul class="sidebar-menu">
+            
+            <!-- Optionally, you can add icons to the links -->
+
+            <li class="active">
+                <a href="http://localhost/sistem-penyewaan/index.php/beranda" class="link">
+                <i class="icon"><img src="http://localhost/sistem-penyewaan/assets/img/home.png" alt="home"></i>
+                Beranda
+                </a>
+            </li>
+            <li class="active">
+                <a href="http://localhost/sistem-penyewaan/index.php/tambah_kendaraan" class="link">
+                <i class="icon"><img src="http://localhost/sistem-penyewaan/assets/img/Plus.png" alt="home"></i>
+                Tambah Kendaraan
+                </a>
+            </li>
+            <li class="active">
+                <a href="http://localhost/sistem-penyewaan/index.php/data_mobil" class="link">
+                <i class="icon"><img src="http://localhost/sistem-penyewaan/assets/img/car-side.png" alt="home"></i>
+                Data Mobil
+                </a>
+            </li>
+            <li class="active">
+                <a href="http://localhost/sistem-penyewaan/index.php/pemesanan" class="link">
+                <i class="icon"><img src="http://localhost/sistem-penyewaan/assets/img/user-cart.png" alt="home"></i>
+                Pesanan Kendaraan
+                </a>
+            </li>
+            <li class="active">
+                <a href="http://localhost/sistem-penyewaan/index.php/profil_pt" class="link">
+                <i class="icon"><img src="http://localhost/sistem-penyewaan/assets/img/profile.png" alt="home"></i>
+                Profil PT
+                </a>
+            </li>
+          
+        </ul>
+        <div class="logout">
+           <p> Log Out</p>
+        </div>
+        </div>
+        <!-- /.sidebar-menu -->
+       
+    <!-- /.sidebar -->
+    </aside>   
