096c3eec5846641d273559193767c8bc5f7221a2
Abdullah Rusyda Maskur <96782892+AbdullahRusydaMaskur@users.noreply.github.com>
2023-12-30 06:01:32 +0700

    Update m_datamobil.php

--- application/models/m_datamobil.php
+++ application/models/m_datamobil.php
@@ -8,26 +8,16 @@ class M_datamobil extends CI_Model {
 		return $data->result_array();
 	}
 	
-	public function select_all_owner() {		
-		$data = $this->db->get("owner");
-		return $data->result_array();
-	}
 	public function get_by_plat($plat) {
 		$plat_escaped = str_replace("%20", " ", $plat);
 		$data = $this->db->get_where('mobil', ['plat' => $plat_escaped])->row_array();
 		return $data;
-		// echo "<pre>";
-        // print_r($data);
-        // echo "</pre>";
     }
+	
 	public function update_mobil($data, $plat) {
 		$this->db->where('plat', $plat);
 		$this->db->update('mobil', $data);
 	}
-		// public function update($data, $plat) {
-		//     $this->db->update('mobil', $data, ['plat' => $plat]);
-		//     return $this->db->affected_rows();
-		// }
 	
 	public function delete_by_plat($plat_escaped){
 		$this->db->where('plat', $plat_escaped);
@@ -41,9 +31,6 @@ class M_datamobil extends CI_Model {
 	public function detail(){
         return $this->db->get_where('owner',['id_owner'=>'1']);
     }
-	// public function detail_mobil(){
-    //     return $this->db->get_where('mobil',['plat'=>$plat_escaped]);
-    // }
 
 	public function select_all_join()
 	{
